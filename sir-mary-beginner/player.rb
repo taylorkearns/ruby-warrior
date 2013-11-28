6e2fd9b6db2ea13c6a662c6a5bd81c8359698940
Taylor Kearns <tkearns@gmail.com>
2013-11-28 10:34:55 -0500

    Rework previous health logic WIP

--- sir-mary-beginner/player.rb
+++ sir-mary-beginner/player.rb
@@ -8,14 +8,14 @@ class Player
 
   delegate [:attack!, :walk!, :feel] => :warrior
 
-  attr_accessor :warrior, :health
+  attr_accessor :warrior
 
   PRIORITIES = [::Retreater, ::Attacker, ::Walker]
   MAX_HEALTH = 20
 
   def initialize
     @warrior = {}
-    @health = health
+    @previous_health = MAX_HEALTH
   end
 
   def play_turn(warrior)
@@ -25,11 +25,11 @@ class Player
 
     priority.new(self).perform_action
 
-    health = warrior.health
+    @previous_health = warrior.health
   end
 
   def low_health?
-    health <= low_health_threshold
+    warrior.health <= low_health_threshold
   end
 
   def next_to_enemy?
@@ -40,12 +40,16 @@ class Player
     Feeler.new(self).space_available?
   end
 
+  def taking_damage?
+    warrior.health < @previous_health
+  end
+
   def low_health_threshold
-    #return last_hit * 2 if last_hit
+    return last_hit * 2 if taking_damage?
     6
   end
 
-  def health
-    @_health ||= MAX_HEALTH
+  def last_hit
+    @previous_health - warrior.health
   end
 end
--- sir-mary-beginner/spec/player_spec.rb
+++ sir-mary-beginner/spec/player_spec.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 require 'spec_helper'
 require_relative '../player'
 
@@ -7,18 +8,30 @@ describe Player do
   end
 
   describe '#low_health?' do
-    before do
+    it 'returns true when health is at or below the danger threshold' do
+      @player.stub(:starting_health) { 6 }
+      @player.stub(:previous_health) { 6 }
 
+      expect(@player.low_health?).to be_true
     end
 
-    it 'returns true when health is at or below the danger threshold' do
-      @player.stub(:health) { 6 }
+    it 'returns true when health is at or below the danger threshold when taking damage' do
+      @player.stub(:starting_health) { 7 }
+      @player.stub(:previous_health) { 11 }
 
       expect(@player.low_health?).to be_true
     end
 
     it 'returns false when health is above the danger threshold' do
-      @player.stub(:health) { 12 }
+      @player.stub(:starting_health) { 8 }
+      @player.stub(:previous_health) { 8 }
+
+      expect(@player.low_health?).to be_false
+    end
+
+    it 'returns true when health is above the danger threshold when taking damage' do
+      @player.stub(:starting_health) { 5 }
+      @player.stub(:previous_health) { 7 }
 
       expect(@player.low_health?).to be_false
     end
