48642681297c0c370b55fa39304ac8de7f0fd7c9
Taylor Kearns <tkearns@gmail.com>
2013-12-05 09:52:20 -0500

    Refactor clear_shot_at_enemy? on Attacker

--- sir-mary-beginner/attacker.rb
+++ sir-mary-beginner/attacker.rb
@@ -22,20 +22,14 @@ class Attacker
   private
 
   def clear_shot_at_enemy?
-    clear_shot = nil
-
     visible_spaces.each do |space|
       if enemy_at?(space)
         return true
       elsif captive_at?(space)
         return false
-      else
-        next
       end
-
-      return clear_shot || false
     end
 
-    clear_shot
+    false
   end
 end
